00ed5de0ce4aa2d381d200db5b420ae566ec2820
Lecter <leandro_valenzuela@hotmail.com>
2019-06-29 23:53:28 -0400

    envioCorreo

--- ProbandoEnvioCorreo/Program.cs
+++ ProbandoEnvioCorreo/Program.cs
@@ -6,8 +6,71 @@ namespace ProbandoEnvioCorreo
 {
     class Program
     {
+
+        static string correoReceptor;
+        static string asuntoMensaje;
+        static string contenidoMensaje;
+        static string copia;
         static void Main(string[] args)
         {
+
+
+            // ingreso de parametros
+            Console.Write("Ingrese correo receptor: ");
+            correoReceptor = Console.ReadLine();
+            Console.Write("Ingrese el asunto del mensaje: ");
+            asuntoMensaje = Console.ReadLine();
+            Console.Write("Ingrese el contenido del mensaje: ");
+            asuntoMensaje = Console.ReadLine();
+            Console.Write("Ingrese la copia del mensaje: ");
+            copia = Console.ReadLine();
+            envioCorreo(correoReceptor, asuntoMensaje, contenidoMensaje, copia);
+
+
+        } // fin main
+
+        
+        static void envioCorreo(string receptor, string asunto, string contenido, string copia)
+        {
+            // informacion
+            System.Net.Mail.MailMessage mensaje = new System.Net.Mail.MailMessage();
+
+            mensaje.To.Add(receptor); // receptor
+            mensaje.Subject = asunto; // asunto del mensaje
+            mensaje.SubjectEncoding = System.Text.Encoding.UTF8; // encoding para servidores
+            mensaje.Bcc.Add(copia);
+
+
+            mensaje.Body = contenido; // cuenrpo mensaje
+            mensaje.BodyEncoding = System.Text.Encoding.UTF8;
+            mensaje.IsBodyHtml = true;
+            mensaje.From = new System.Net.Mail.MailAddress("leandro.valenzuela02@inacapmail.cl"); // mi correo 
+
+            System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
+
+            cliente.Credentials = new System.Net.NetworkCredential("leandro.valenzuela02@inacapmail.cl", ""); // correo y contraseña
+
+            cliente.Port = 587; // puerto
+            cliente.EnableSsl = true; // seguridad ssl
+
+            cliente.Host = "SMTP.Office365.com"; // servidor de salida
+
+            try
+            {
+                cliente.Send(mensaje);
+                Console.WriteLine("Mensaje enviado!!");
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Maaal: " + ex);
+                Console.ReadKey();
+            }
+
+        
         }
-    }
-}
+
+
+    } // fin class
+
+} // fin namespace
